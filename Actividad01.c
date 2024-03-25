a79c12265c32e4640d2a8c27a32df5ab3fe83471
MejiaEynar <eynarmejia03@gmail.com>
2024-03-25 19:27:07 -0300

    Estructura del programa

--- Actividad01.c
+++ Actividad01.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
+void posOneg(){
+
+}
+
+void parOinpar(){
+
+}
+
+void Multiplo2o3(){
+
+}
+
+void Mayorque(){
+
+}
+
 int main(){
-    
+    switch(){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            printf("Opcion no Valida\n");
+            break;
+    }
 }
