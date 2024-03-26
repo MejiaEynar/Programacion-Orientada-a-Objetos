181666a393182c2ce56ff33e2010be2a1d95c241
MejiaEynar <eynarmejia03@gmail.com>
2024-03-26 14:28:31 -0300

    Ejercicio part(2) numero 2 completo

--- ActRep2603.c
+++ ActRep2603.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(){
-    
+void promediode3(float n1, float n2, float n3){
+    float t = (n1 + n2 + n3) / 3;
+    printf("El promedio de estos 3 numeros es: %.2f\n", t);
+}
+
+int main(void){
+    float n1, n2, n3;
+    scanf("%f %f %f", &n1, &n2, &n3);
+    promediode3(n1, n2, n3);
     return 0;
 }
