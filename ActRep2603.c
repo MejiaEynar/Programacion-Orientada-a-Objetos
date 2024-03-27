cdbb522373f2aaa69e54366c561636a698bd4933
MejiaEynar <eynarmejia03@gmail.com>
2024-03-27 11:46:25 -0300

    Ejercicio 5 completo

--- ActRep2603.c
+++ ActRep2603.c
@@ -7,6 +7,7 @@ void promediode3(float n1, float n2, float n3){
     float t = (n1 + n2 + n3) / 3;
     printf("El promedio de estos 3 numeros es: %.2f\n", t);
 }
+
 // Ejercicio N°3
 void parOinparmayorque0(int n){
     if (n > 0) {
@@ -19,6 +20,7 @@ void parOinparmayorque0(int n){
         printf("El número ingresado no es válido. Debe ser positivo y mayor que cero.\n");
     }
 }
+
 // Ejercicio 4
 void apro(float not1, float not2, float not3, float not4, float tarea){
     float notA = (not1 + not2 + not3 + not4 + tarea) / 5;
@@ -33,10 +35,26 @@ void apro(float not1, float not2, float not3, float not4, float tarea){
     }
 }
 
+// Ejercicio 5
+void apro2(float nota){
+    if(nota >= 8){
+        printf("El alumno aprobo\n");
+        printf("Felicitaciones\n");
+    }else if(nota >= 6){
+        printf("El alumno aprobo\n");
+    } else if(nota = 1){
+        printf("El alumno no aprobo\n");
+        printf("Debes esforzarte más\n");
+    } else if(nota <= 5){
+        printf("El alumno no aprobo\n");
+    } 
+}
+
 int main(void){
     float n1, n2, n3; //Variable del ejercicio 2
     int n; //Variable del ejercicio 3
-    float not1, not2, not3, not4, tarea;
+    float not1, not2, not3, not4, tarea; // Variables jercicio 4
+    int nota;
     int opcion;
     scanf("%d", &opcion);
     switch(opcion){
@@ -64,6 +82,8 @@ int main(void){
             apro(not1, not2, not3, not4, tarea);
             break;
         case 5:
+            scanf("%d", %nota);
+            apro2(nota);
             break;
         default:
             break;
