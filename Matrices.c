761f9f31ff7982368aa8bafc5aba082c5836609b
MejiaEynar <eynarmejia03@gmail.com>
2024-04-15 18:22:06 -0300

    Respuesta opcion 2

--- Matrices.c
+++ Matrices.c
@@ -33,10 +33,14 @@ main ()
 }
 
 /*
-No entendi bien si queria que se indicara las formas en las que se podia optimizar este codigo
-o si queria que en una "caja" como esta. Escribieramos en codigo los cambios a hacer.
+
+	No entendi bien si queria que se indicara las formas en las que se podia optimizar este codigo
+	o si queria que en una "caja" como esta. Escribieramos en codigo los cambios a hacer.
+
 */
+
 /*
+
 Opcion 1(No codigo):
 	Lo que se podria hacer para mejorar el codigo para que este aceptara matrices de un tamaño
 	ingresado por el usuario. A travez de una funcion haciendo uso del scanf para obtener el tamaño 
@@ -44,8 +48,45 @@ Opcion 1(No codigo):
 	los valores que serian ingresados en la matriz. Tambien se podria dar a escojer al usuario si este quiere rellenar
 	la matriz de forma manual o que esta se rellene automaticamente con numeros aleatorios.
 */
+
 /*
+
 Opcion 2(codigo):
 
+#include<stdio.h>
+
+void imprimirMatriz(int f, int c, int A[f][c]) {
+    int i, j;
+    printf("La matriz ingresada es:\n");
+    for (i = 0; i < f; i++) {
+        for (j = 0; j < c; j++) {
+            printf("%d\t", A[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int f, c;
+    int i, j;
+
+    printf("Ingrese el número de filas de la matriz: ");
+    scanf("%d", &f);
+    printf("Ingrese el número de columnas de la matriz: ");
+    scanf("%d", &c);
+
+    int A[f][c];
+
+    printf("Ingrese los elementos de la matriz:\n");
+    for (i = 0; i < f; i++) {
+        for (j = 0; j < c; j++) {
+            printf("Ingrese el valor A[%d][%d]: ", i, j);
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    imprimirMatriz(f, c, A);
+
+}
 
 */
