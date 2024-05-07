dc60be34e9c4a37f97669e2444477b3d8d739335
MejiaEynar <eynarmejia03@gmail.com>
2024-05-07 14:48:06 -0300

    Una vez el codigo este listo, sera pasado a c++

--- JuegoPirata.cpp
+++ JuegoPirata.cpp
@@ -2,10 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-void iniciarTablero(){}
-
-void dibujarTablero(int c, int f){
+int dibujarTablero(int c, int f){
     for(int i = 0;i < f; i++){
         printf("[ ");
         for(int j = 0;j < c; j++){
@@ -15,17 +12,40 @@ void dibujarTablero(int c, int f){
     }
 }
 
-void BuscarTesoro(){}
-
 int main(){
     srand(time(NULL));
-    int f, c;
-    printf("\nIngrese el numero de Filas:");
-    scanf("%d", &c);
-    printf("\nIngrese el numero de Columnas:");
-    scanf("%d", &f);
-    iniciarTablero(c, f);
-    dibujarTablero();
-    BuscarTesoro();
-    
+    int f, c, op;
+    int Matriz[f][c];
+    scanf("%d", &op);
+    do{
+    switch(op){
+        case 1:
+            do{
+                printf("\nIngrese el numero de Columnas: ");
+                if (scanf("%d", &c) != 1 || c < 1){
+                        printf("Opcion invalida, ingrese una opcion valida.\n");
+                    while (getchar() != '\n');
+                } else {
+                    break;
+                }
+            } while (1);
+            do{
+                printf("\nIngrese el numero de Filas: ");
+                if (scanf("%d", &f) != 1 || f < 1){
+                        printf("Opcion invalida, ingrese una opcion valida.\n");
+                    while (getchar() != '\n');
+                } else {
+                    break;
+                }
+            } while (1);
+            dibujarTablero(c,f);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            break;
+    }
+    }while(op != 3);
 }
