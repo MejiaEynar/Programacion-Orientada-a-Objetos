bfed51b03f22b04d5c8c8b588cf960293928b59d
MejiaEynar <eynarmejia03@gmail.com>
2024-05-28 15:18:43 -0300

    Juego en funcionamiento. Correcciones a hacer

--- TresRaya.c
+++ TresRaya.c
@@ -17,18 +17,43 @@ void inicializarTablero(char tablero[f][c]){
     }
 }
 
-void dibujarTablero(char tablero[f][c]){
+void dibujarTablero(char tablero[f][c], char jugador){
     printf("\n");
     for (int i = 0; i < f; i++) {
-        printf(" %c | %c | %c \n", tablero[i][0], tablero[i][1], tablero[i][2]);
+       if(jugador = 'X'){
+           printf("\x1B[31m %c \x1B[37m|\x1B[37m %c \x1B[37m|\x1B[37m %c \x1B[31m\n", tablero[i][0], tablero[i][1], tablero[i][2]);
+        } else{
+            printf("\x1B[34m %c \x1B[37m|\x1B[37m %c \x1B[37m|\x1B[37m %c \x1B[34m\n", tablero[i][0], tablero[i][1], tablero[i][2]);
+        }
         if (i != f - 1)
-            printf("---|---|---\n");
+            printf("\x1B[37m ---|---|--- \x1B[37m\n");
     }
 }
 
-void verificarGanador(){}
+int verificarGanador(char tablero[f][c], char jugador){
+    for (int i = 0; i < f; i++) {
+        if (tablero[i][0] == jugador && tablero[i][1] == jugador && tablero[i][2] == jugador)
+            return 1; 
+        if (tablero[0][i] == jugador && tablero[1][i] == jugador && tablero[2][i] == jugador)
+            return 1; 
+    }
 
-void tableroCompleto(){}
+    if ((tablero[0][0] == jugador && tablero[1][1] == jugador && tablero[2][2] == jugador) ||
+        (tablero[0][2] == jugador && tablero[1][1] == jugador && tablero[2][0] == jugador))
+        return 1; 
+
+    return 0; 
+}
+
+int tableroCompleto(char tablero[f][c]){
+    for (int i = 0; i < f; i++) {
+        for (int j = 0; j < c; j++) {
+            if (tablero[i][j] == ' ')
+                return 0; 
+        }
+    }
+    return 1;
+}
 
 int main(){
     srand(time(NULL));
@@ -36,7 +61,7 @@ int main(){
     int fila, columna;
     char jugador_actual = 'X';
     int op;
-    printf("¡Bienvenido al TA TE TI\n");
+    printf("¡Bienvenido al TA TE TI!\n");
     do{
         printf("\nMenú:\n");
         printf("1) Jugar\n");
@@ -49,7 +74,7 @@ int main(){
             case 1:
                 inicializarTablero(tablero);
                 printf("\n");
-                dibujarTablero(tablero);
+                dibujarTablero(tablero, jugador_actual);
                 while (1) {
                     printf("Turno del jugador %c. Ingrese la fila y columna: ", jugador_actual);
                     scanf("%d %d", &fila, &columna);
@@ -62,7 +87,7 @@ int main(){
                     if (tablero[fila - 1][columna - 1] == ' ') {
                         tablero[fila - 1][columna - 1] = jugador_actual;
                         limpiarPantalla();
-                        dibujarTablero(tablero);
+                        dibujarTablero(tablero, jugador_actual);
 
                         if (verificarGanador(tablero, jugador_actual)) {
                             printf("¡El jugador %c ha ganado!\n", jugador_actual);
